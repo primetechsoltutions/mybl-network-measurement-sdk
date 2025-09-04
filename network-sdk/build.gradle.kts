305309046f0799c32f77986030a535dcac31748a
abusufianshibli <abu35-1911@diu.edu.bd>
2025-09-04 17:54:21 +0600

    remove src file jar and add live base url as retailer app.

--- network-sdk/build.gradle.kts
+++ network-sdk/build.gradle.kts
@@ -36,12 +36,8 @@ android {
     }
     publishing {
         // expose both variants for publishing
-        singleVariant("release") {
-            withSourcesJar()
-        }
-        singleVariant("debug") {
-            withSourcesJar()
-        }
+        singleVariant("release") {}
+        singleVariant("debug") {}
     }
 
     buildTypes {
